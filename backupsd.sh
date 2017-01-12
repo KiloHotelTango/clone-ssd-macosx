94cd0c4975f2cdfcc8a8fce70d27e8e8272e279c
Abdelkarim Mateos <abdelkarim@tamainut.com>
2017-01-12 08:27:40 +0100

    Ayuda y eliminación del sudo

--- backupsd.sh
+++ backupsd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # script to backup Pi SD card
 # Localiza el disco con una partición linux (como la de Raspbian)
+# Ejecútelo desde una terminal usando  sudo backupsd.sh
 
 
 export DSK=`diskutil list | grep "Linux" | cut -c 69-73`
@@ -16,8 +17,8 @@ diskutil unmountDisk /dev/$DSK
 echo Por favor, espere. Esto toma algún tiempo
 # requiere el comando PV. Sino, puede des-comentar las lineas de abajo y comente la que contiene pv
 #echo Ctl+T para ver el progreso
-#time sudo dd if=/dev/r$DSK bs=1m | gzip -9 > ~/Desktop/Piback.img.gz
-time sudo dd if=/dev/r$DSK bs=1m | pv | gzip -9 > ~/Desktop/Piback.img.gz
+#time dd if=/dev/r$DSK bs=1m | gzip -9 > ~/Desktop/Piback.img.gz
+time dd if=/dev/r$DSK bs=4m | pv | gzip -9 > ~/Desktop/Piback.img.gz
 
 # Renombrar el backup a la fecha actual
 echo Compresion completa - Ahora renombramos
